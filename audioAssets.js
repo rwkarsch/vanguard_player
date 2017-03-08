b663313d8aa112252aa91230ff07f4c726c10a82
rwkarsch <robertkarsch@gmail.com>
2017-03-07 23:18:49 -0500

    Initial add

---
+++ audioAssets.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title' : 'Vanguard 2017 Economic Outlook',
+        'url' : 'https://s3.amazonaws.com/rwkarschbucket/3804_ICPodcast022017.mp3'
+    },
+    {
+        'title' : 'Do better by doing good - Charitable Giving',
+        'url' : 'https://s3.amazonaws.com/rwkarschbucket/3794_ICPodcast012017.mp3'
+    },
+    {
+        'title' : 'Invest now or temporarily hold your cash?',
+        'url' : 'https://s3.amazonaws.com/rwkarschbucket/3745_ICPodcast_Invest_Now_or_Hold.mp3'
+    }
+];
+
+module.exports = audioData;
